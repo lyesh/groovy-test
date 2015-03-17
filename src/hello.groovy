635ab60879520931af9aa4a48d89b19a84e06c71
Ailish Holmes <aholmes@2m-rzacher.healthmetrics.org>
2015-03-17 18:20:12 -0400

    Initial commit

---
+++ src/hello.groovy
@@ -0,0 +1,13 @@
+/**
+ * Created by aholmes on 3/17/15.
+ */
+import groovy.lang.Binding;
+import groovy.lang.Script;
+
+
+public class hello {
+    public static void main(String[] args) {
+        println("hola")
+
+    }
+}
